86e45cf229cb68da823694b5b0374264bf37cd3e
krishna <dhekkrishna1@gmail.com>
2023-11-09 20:20:15 +0530

    Used Annotation Value to inject the custom peoperties defined in the application.properties file, these properties are displayed using the endpoint teaminfo

--- 01-spring-boot-demo/src/main/java/com/springboot/demo/myCoolApp/rest/FunController.java
+++ 01-spring-boot-demo/src/main/java/com/springboot/demo/myCoolApp/rest/FunController.java
@@ -1,5 +1,6 @@
 package com.springboot.demo.myCoolApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,18 @@ public class FunController {
     @GetMapping("/fortune")
     public String getDailyFortune(){
         return "Today is your lucky day!";
+    }
+
+    //@Value annotation to inject values from properties "coach.name" and "team.name"
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: "+ coachName + " , TeamName : " +teamName;
     }
 
 }
