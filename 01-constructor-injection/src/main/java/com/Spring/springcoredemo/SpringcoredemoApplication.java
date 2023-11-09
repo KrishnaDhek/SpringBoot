331704f1f9f791abab6a35e3c5311269c9e0bae7
krishna <dhekkrishna1@gmail.com>
2023-11-09 22:06:40 +0530

    Implemented constructor injection for dailyworkout endpoint, we declard an interface coach with a getDailyWorkout method, a class CricketCoach that implements Coach interface and  usied @Component annotation to mark CricketCoach  as  spring bean , a DemoController  with constructor injection using @Autowired annotation, defined a HTTP endpoint dailyworkout for providing workout instruction

--- 01-constructor-injection/src/main/java/com/Spring/springcoredemo/SpringcoredemoApplication.java
+++ 01-constructor-injection/src/main/java/com/Spring/springcoredemo/SpringcoredemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringcoredemoApplication.class, args);
 	}
 
