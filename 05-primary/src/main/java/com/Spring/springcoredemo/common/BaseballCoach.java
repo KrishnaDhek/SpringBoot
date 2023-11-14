060cea6b1c1bce5a1966646a08ca1b2d0f493e71
krishna <dhekkrishna1@gmail.com>
2023-11-14 13:31:16 +0530

    Using @Primary annotation to indicate a default bean when multiple bean are implementing a interface

--- 05-primary/src/main/java/com/Spring/springcoredemo/common/BaseballCoach.java
+++ 05-primary/src/main/java/com/Spring/springcoredemo/common/BaseballCoach.java
@@ -1,8 +1,10 @@
 package com.Spring.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class BaseballCoach implements Coach{
     @Override
     public String getDailyWorkout() {
