fe34117e248728aebf51c0e1d24bd26ba3a93670
krishna <dhekkrishna1@gmail.com>
2023-11-19 22:27:43 +0530

    Add CommandLineRunner bean for application startup
    In the CruddemoApplication class, a CommandLineRunner bean named 'commandLineRunner' is added using the @Bean annotation. This bean executes a simple 'Hello World' message when the application starts. This can be useful for performing initialization tasks or running code at the application's startup.

--- 10-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/hibernate/cruddemo/CruddemoApplication.java
+++ 10-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/hibernate/cruddemo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.spring.hibernate.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,4 +12,13 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	@Bean
+
+	// CommandLineRunner bean to execute code when the application starts
+	public CommandLineRunner commandLineRunner(String[] args){
+	return runner->{
+		// Code to be executed when the application starts
+		System.out.println("Hello World");
+	};
+	}
 }
