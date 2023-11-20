e0ef9db6eadbca18b61e12052fd9e1dc79792800
krishna <dhekkrishna1@gmail.com>
2023-11-20 22:05:27 +0530

    Overried the StudentDAO interface method update inside class StudentDAOImpl

--- 10-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/hibernate/cruddemo/dao/StudentDAOImpl.java
+++ 10-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/hibernate/cruddemo/dao/StudentDAOImpl.java
@@ -64,5 +64,12 @@ public class StudentDAOImpl implements StudentDAO{
         return theQuery.getResultList();
     }
 
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+
+    }
+
 
 }
