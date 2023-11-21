7de2d2de099d2cd7831e414b44f41c14093dcdb7
krishna <dhekkrishna1@gmail.com>
2023-11-22 01:39:26 +0530

    Implement EmployeeServiceImpl
    Added the EmployeeServiceImpl class, a service component responsible for managing employee-related business logic and interacting with the data access layer (DAO). This class includes methods for retrieving all employees, finding an employee by ID, saving an employee, and deleting an employee by ID. The @Service annotation designates it as a Spring service component for dependency injection.

--- 11-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 11-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,12 +1,14 @@
 package com.springboot.cruddemo.service;
 
-import com.springboot.cruddemo.dao.EmployeeDAO;
+
+import com.springboot.cruddemo.dao.EmployeeRepository;
 import com.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Service class for handling employee-related business logic and interaction with the DAO layer.
@@ -15,36 +17,44 @@ import java.util.List;
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
     // DAO for accessing employee data
-
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     // Constructor-based dependency injection
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
-        employeeDAO = theEmployeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository){
+        employeeRepository = theEmployeeRepository;
     }
     // Implementation of the findAll method from the EmployeeService interface
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee =null;
+        if(result.isPresent()){
+            theEmployee =result.get();
+        }
+        else {
+            throw new RuntimeException("Did not find employee Id "+theId);
+        }
+        return theEmployee;
     }
 
-    @Transactional
+
     @Override
     public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
-    @Transactional
+
     @Override
     public void deleteById(int theId) {
-        employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
 
 
