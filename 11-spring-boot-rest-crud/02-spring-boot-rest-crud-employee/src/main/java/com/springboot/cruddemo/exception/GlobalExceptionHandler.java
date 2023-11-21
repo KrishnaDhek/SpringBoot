2ba14b75da93fb5e57b6d13ce58c64fb4e63f501
krishna <dhekkrishna1@gmail.com>
2023-11-21 23:05:02 +0530

    feat: Add global exception handling for EmployeeNotFoundException and EmployeeInvalidException
    **Comments:**
    
    1.  class:
       - This class represents the structure of the error response sent to clients.
       - It includes fields for status, message, and timestamp.
    
    2.  class:
       - Extends  and is used to represent exceptions related to invalid employee data.
    
    3.  class:
       - Extends  and is used to represent exceptions when an employee is not found.
    
    4.  class:
       - Annotates with  to make it a global exception handler for all controllers.
       - Contains methods annotated with  to handle specific exception types.
       -  handles  and returns a NOT_FOUND response.
       -  handles  and returns a BAD_REQUEST response.
    
    5. The status codes in the  are set based on the HTTP status codes (e.g., NOT_FOUND, BAD_REQUEST).

--- 11-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springboot/cruddemo/exception/GlobalExceptionHandler.java
+++ 11-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springboot/cruddemo/exception/GlobalExceptionHandler.java
@@ -8,23 +8,41 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Exception handler for EmployeeNotFoundException
     @ExceptionHandler
     public ResponseEntity<EmployeeErrorResponse> handleEmployeeNotFoundException(EmployeeNotFoundException exc){
+        // Creating an instance of EmployeeErrorResponse
         EmployeeErrorResponse error = new EmployeeErrorResponse();
+
+        // Setting response status to 404 (Not Found)
         error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        // Setting error message to the message from the exception
         error.setMessage(exc.getMessage());
+
+        // Setting timestamp to the current system time
         error.setTimeStamp(System.currentTimeMillis());
 
+        // Returning the error response with a 404 status
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
 
+    // Exception handler for EmployeeInvalidException
     @ExceptionHandler
     public ResponseEntity<EmployeeErrorResponse> handleEmployeeInvalidException(EmployeeInvalidException exc){
+        // Creating an instance of EmployeeErrorResponse
         EmployeeErrorResponse error = new EmployeeErrorResponse();
+
+        // Setting response status to 400 (Bad Request)
         error.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        // Setting a generic error message for invalid employee ID
         error.setMessage("Required an integer value for employeeId");
+
+        // Setting timestamp to the current system time
         error.setTimeStamp(System.currentTimeMillis());
 
+        // Returning the error response with a 400 status
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
