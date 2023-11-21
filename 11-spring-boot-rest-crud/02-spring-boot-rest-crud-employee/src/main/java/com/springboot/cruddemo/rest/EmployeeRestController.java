d1b12018cd42ff1114dad98933bb103849902243
krishna <dhekkrishna1@gmail.com>
2023-11-21 21:16:20 +0530

    Added EmployeeRestController with endpoints for adding  a new employee via POST request.

--- 11-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springboot/cruddemo/rest/EmployeeRestController.java
+++ 11-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,11 +3,9 @@ package com.springboot.cruddemo.rest;
 
 import com.springboot.cruddemo.entity.Employee;
 import com.springboot.cruddemo.service.EmployeeService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,14 +27,30 @@ public class EmployeeRestController {
        return employeeService.findAll();
     }
 
-    //expose endpoint "/employees/{employeeId}"
+    //add mapping for getting  employee via id
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId ){
+
+        // Retrieve the employee from the service by ID
         Employee theEmployee = employeeService.findById(employeeId);
 
+        // If the employee is not found, throw a runtime exception
         if(theEmployee==null) {
             throw new RuntimeException("Employee not found -" + employeeId);
         }
         return theEmployee;
+    }
+
+    // Adding mapping to handle POST requests to add a new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        // Setting ID to 0 to ensure it's treated as a new employee
+        theEmployee.setId(0);
+
+        // Saving the new employee using the service
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        // Returning the saved employee with its assigned ID
+        return dbEmployee;
     }
 }
