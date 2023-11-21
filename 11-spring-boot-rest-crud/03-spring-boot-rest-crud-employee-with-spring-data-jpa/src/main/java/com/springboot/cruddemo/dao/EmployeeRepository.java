caf58700a011798d14bf4754ccda538fbc4ed2d4
krishna <dhekkrishna1@gmail.com>
2023-11-22 01:36:56 +0530

    Create EmployeeRepository interface
    Extends JpaRepository for the Employee entity, leveraging Spring Data JPA's generic CRUD operations. The interface is parameterized with the Employee entity and the type of its primary key (Integer). No additional code implementation is required.

---
+++ 11-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.cruddemo.dao;
+
+import com.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The EmployeeRepository interface extends JpaRepository for the Employee entity.
+ * It leverages Spring Data JPA's generic CRUD operations, eliminating the need for explicit implementations.
+ * The interface is parameterized with the Employee entity and the type of its primary key (Integer).
+ */
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    //no need to write any code
+}
