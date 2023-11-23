4f96201d13dd472308b502f70ab5867ecea1faa2
krishna <dhekkrishna1@gmail.com>
2023-11-23 17:44:02 +0530

    Add form processing in HelloWorldController
    - Created a new controller method 'readData' to handle form submissions and process the input.
    - Modified 'helloworld.html' to display the submitted student name and a processed message.
    - Added a new Thymeleaf template 'helloworld-form.html' for the input form.

--- 13-spring-boot-spring-mvc/thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 13-spring-boot-spring-mvc/thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -20,4 +23,22 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    //need a controller method to read form data
+    @GetMapping("/processFormVersionTwo")
+    public String readData(HttpServletRequest request, Model model){
+
+        //read request parameter from html form
+        String name = request.getParameter("studentName");
+
+        //convert data to UpperCase
+        name = name.toUpperCase();
+
+        //create the message
+        String result = "Yo! " +name;
+
+        //add message to the model
+        model.addAttribute("message",result);
+
+        return "helloworld";
+    }
 }
