a202b28b9f65375f0d21c74eb24c3b695dcc8d55
krishna <dhekkrishna1@gmail.com>
2023-11-24 21:43:16 +0530

    Add StudentController for handling student form, including form display and submission processing

--- 13-spring-boot-spring-mvc/thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/controller/StudentController.java
+++ 13-spring-boot-spring-mvc/thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,27 +1,51 @@
 package com.springboot.thymeleafdemo.controller;
 
 import com.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
 
-    //method to show form
+    // Adding countries, gender, language, and operatingSystem as values from application.properties
+    @Value("${countries}")
+    private List<String>countries;
+
+    @Value("${gender}")
+    private List<String>gender;
+
+    @Value("${language}")
+    private List<String>language;
+
+    @Value("${operatingSystem}")
+    private List<String> operatingSystem;
+
+    // Method to show the student form
     @GetMapping("/showStudentForm")
     public String showForm(Model themodel){
 
         //create Student object
         Student theStudent = new Student();
 
-        //add student object to model
+        // Add the student object and other lists to the model
         themodel.addAttribute("student", theStudent);
+        themodel.addAttribute("countries", countries);
+        themodel.addAttribute("gender", gender);
+        themodel.addAttribute("language", language);
+        themodel.addAttribute("operatingSystem", operatingSystem);
+
+        // Return the view name for rendering the student form
         return "student-form";
     }
 
+
+    // Method to process the student form submission
 
     @PostMapping("/processStudentForm")
     public String processForm(@ModelAttribute("student")Student theStudent){
@@ -29,6 +53,7 @@ public class StudentController {
         //log the input data
         System.out.println("theStudent :" +theStudent.getFirstName()+" "+theStudent.getLastName());
 
+        // Return the view name for rendering the confirmation page
         return "student-confirmation";
     }
 }
