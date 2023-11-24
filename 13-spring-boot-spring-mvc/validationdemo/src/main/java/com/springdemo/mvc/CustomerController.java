1ff4db343c2d10a489405455c58c6f3090b59709
krishna <dhekkrishna1@gmail.com>
2023-11-24 22:52:16 +0530

    Add StringTrimmerEditor in initBinder to automatically trim input strings
    - Use @InitBinder to register a StringTrimmerEditor to remove leading and trailing whitespaces.
    - Enhance the customer form submission handling in processForm method.

--- 13-spring-boot-spring-mvc/validationdemo/src/main/java/com/springdemo/mvc/CustomerController.java
+++ 13-spring-boot-spring-mvc/validationdemo/src/main/java/com/springdemo/mvc/CustomerController.java
@@ -1,15 +1,28 @@
 package com.springdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    //add an initBinder.... to convert the trim input string
+    //remove leading and trailing whitespaces
+    //resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String shoeForm(Model theModel){
@@ -22,6 +35,7 @@ public class CustomerController {
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
         BindingResult theBindingResult){
 
+        System.out.println("Last Name : |" + theCustomer.getLastName() +"|");
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }
