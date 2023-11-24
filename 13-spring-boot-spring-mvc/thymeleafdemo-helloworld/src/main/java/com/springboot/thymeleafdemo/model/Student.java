ae07933693e874b992dd1f87119a5d06fdedafe6
krishna <dhekkrishna1@gmail.com>
2023-11-24 21:37:45 +0530

    Added fields for language, country, gender and operating system, implemented getter and detter for the same

--- 13-spring-boot-spring-mvc/thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/model/Student.java
+++ 13-spring-boot-spring-mvc/thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/model/Student.java
@@ -1,13 +1,56 @@
 package com.springboot.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
 
     private String lastName;
 
+    private String gender;
+
+    private String country;
+
+    private String language;
+
+    private List<String> operatingSystem;
+
     public Student(){
 
+    }
+
+
+    public List<String> getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(List<String> operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public String getFirstName() {
