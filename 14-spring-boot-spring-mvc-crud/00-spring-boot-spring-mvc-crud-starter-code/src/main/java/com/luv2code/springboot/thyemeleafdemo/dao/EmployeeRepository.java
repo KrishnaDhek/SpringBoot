9030f53d97214381dc7d445e2c7c06c22f89577a
krishna <dhekkrishna1@gmail.com>
2023-11-26 00:15:59 +0530

    Add custom query method to EmployeeRepository for sorting employees by last name

--- 14-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thyemeleafdemo/dao/EmployeeRepository.java
+++ 14-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thyemeleafdemo/dao/EmployeeRepository.java
@@ -3,8 +3,14 @@ package com.luv2code.springboot.thyemeleafdemo.dao;
 import com.luv2code.springboot.thyemeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    //add method to sort by last name
+
+    public List<Employee> findAllByOrderByLastName();
 
 }
