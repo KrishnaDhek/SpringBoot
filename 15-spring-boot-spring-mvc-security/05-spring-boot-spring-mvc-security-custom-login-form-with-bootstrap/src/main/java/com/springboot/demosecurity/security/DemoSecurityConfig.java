2b4f89c54248c99f15fc4c44025c60e9f7ee87b8
krishna <dhekkrishna1@gmail.com>
2023-11-26 16:17:40 +0530

    Initial commit for spring-boot-spring-mvc-custom-login-form-with-bootstrap

---
+++ 15-spring-boot-spring-mvc-security/05-spring-boot-spring-mvc-security-custom-login-form-with-bootstrap/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    // Configure in-memory user details manager with three users: john, mary, and susan.
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")//Use {noop} to indicate plain text password (for demo purposes).
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+
+    // Configure the security filter chain with specific rules.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated())//Require authentication for any request.
+
+                .formLogin(form ->
+                        form
+                                // Specify the custom login page.
+                                .loginPage("/showLoginPage")
+                                //no controller request mapping required for this
+                                // URL for processing login.
+                                .loginProcessingUrl("/authenticateTheUser")
+                                // Allow all users to access the login page.
+
+                                .permitAll()
+                );
+
+        return http.build();
+
+    }
+}
