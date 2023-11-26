db3d7ab9a92cd116f2a8cbd3305bb205e1e68184
krishna <dhekkrishna1@gmail.com>
2023-11-26 22:05:39 +0530

    Integrate JDBC for user details retrieval and update application.properties

--- 15-spring-boot-spring-mvc-security/08-spring-boot-spring-mvc-security-user-roles-restrict-access/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 15-spring-boot-spring-mvc-security/08-spring-boot-spring-mvc-security-user-roles-restrict-access/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,39 +3,32 @@ package com.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
-    // Configure in-memory user details manager with three users: john, mary, and susan.
+//add support for JDBC .. no hard code
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")//Use {noop} to indicate plain text password (for demo purposes).
-                .roles("EMPLOYEE")
-                .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return jdbcUserDetailsManager;
     }
+
 
 
     // Configure the security filter chain with specific rules.
@@ -72,4 +65,29 @@ public class DemoSecurityConfig {
         return http.build();
 
     }
+   // Configure in-memory user details manager with three users: john, mary, and susan.
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")//Use {noop} to indicate plain text password (for demo purposes).
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
+
 }
