2ef517203a47b4b9dfbe4d4ca0df68fea63b1361
krishna <dhekkrishna1@gmail.com>
2023-11-28 16:58:40 +0530

    Add a method to create an instructor in the CommandLineRunner. The method uses the AppDAO to save an Instructor entity along with its associated InstructorDetail.

--- 16-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
+++ 16-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.springboot.cruddemo;
 
+import com.springboot.cruddemo.dao.AppDAO;
+import com.springboot.cruddemo.entity.Instructor;
+import com.springboot.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,26 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner->{
-			System.out.println("Hello World");
+		createInstructor(appDAO);
+
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		Instructor tempInstructor = new Instructor("Mohit","Dhek","mohit@gmail.com");
+		InstructorDetail tempInstructorDetail = new InstructorDetail("https://www.night.com","guitar");
+
+		//association
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving the instructor:"+tempInstructor);
+		appDAO.save(tempInstructor);
+		System.out.println("Done!");
+
+
+
 	}
 
 }
