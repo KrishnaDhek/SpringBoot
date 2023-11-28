fc306d1be23385ed52309911eaf019f82d380e87
krishna <dhekkrishna1@gmail.com>
2023-11-28 18:21:04 +0530

    Implemented the deleteInstructorByID() that taked an id, and deleted the instrructor based on the provided id

--- 16-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/springboot/cruddemo/dao/AppDAOImpl.java
+++ 16-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/springboot/cruddemo/dao/AppDAOImpl.java
@@ -30,5 +30,15 @@ public class AppDAOImpl implements AppDAO{
         return entityManager.find(Instructor.class, theId);
     }
 
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+
+        entityManager.remove(tempInstructor);
+
+    }
+
 
 }
