8b2759c2b4f23f71d914223ff8b058c9ca7ec221
krishna <dhekkrishna1@gmail.com>
2023-11-28 18:23:40 +0530

    Initial commit for jpa-one-to-one-bidirectional

---
+++ 16-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
@@ -0,0 +1,59 @@
+package com.springboot.cruddemo;
+
+import com.springboot.cruddemo.dao.AppDAO;
+import com.springboot.cruddemo.entity.Instructor;
+import com.springboot.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		return runner->{
+	//	createInstructor(appDAO);
+//		findInstructor(appDAO);
+		deleteInstructor(appDAO);
+		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId =2;
+
+		System.out.println("Delete instructor id:" +theId);
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Done!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId =3;
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println("Instructor id :"+theId);
+		System.out.println("instructor is :" +tempInstructor);
+		System.out.println("the associated details are :"+tempInstructor.getInstructorDetail());
+	}
+
+
+	private void createInstructor(AppDAO appDAO) {
+		Instructor tempInstructor = new Instructor("Mohit","Dhek","mohit@gmail.com");
+		InstructorDetail tempInstructorDetail = new InstructorDetail("https://www.night.com","guitar");
+
+		//association
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving the instructor:"+tempInstructor);
+		appDAO.save(tempInstructor);
+		System.out.println("Done!");
+
+
+
+	}
+
+}
