af0b1f6f523dbbf41d585da622d6af7caa44e398
krishna <dhekkrishna1@gmail.com>
2023-11-28 23:00:58 +0530

    Updated the @OneToMany relationship in the Instructor class to use FetchType.EAGER for eager loading. Set cascade options for PERSIST, MERGE, DETACH, and REFRESH for the courses collection in the bidirectional one-to-many relationship.

--- 16-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/springboot/cruddemo/entity/Instructor.java
+++ 16-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/springboot/cruddemo/entity/Instructor.java
@@ -28,7 +28,9 @@ public class Instructor {
     private InstructorDetail instructorDetail;
 
 
-    @OneToMany(mappedBy = "instructor",
+    //FetchType.EAGER: Specifies eager loading, meaning that
+    //when an Instructor is retrieved, its courses will be loaded immediately.
+    @OneToMany(mappedBy = "instructor", fetch = FetchType.EAGER,
             cascade=  {CascadeType.PERSIST, CascadeType.MERGE,
                        CascadeType.DETACH, CascadeType.REFRESH})
     private List<Course> courses;
