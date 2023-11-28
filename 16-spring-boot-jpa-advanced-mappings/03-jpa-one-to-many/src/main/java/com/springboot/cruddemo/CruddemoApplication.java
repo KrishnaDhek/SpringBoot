07fdc84f7846179c6d84b06cb77537efcf7c2fb9
krishna <dhekkrishna1@gmail.com>
2023-11-28 23:24:28 +0530

    Add a method to find an instructor by ID and then fetch and associate courses for the instructor with ID 1.
    - Implemented findCoursesForInstructor method in AppDAO
    - Prints the instructor details and associated courses

--- 16-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
+++ 16-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/springboot/cruddemo/CruddemoApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -27,8 +29,26 @@ public class CruddemoApplication {
 			//	deleteInstructorDetail(appDAO);
 
 			//createInstructorWithCourses(appDAO);
-			findInstructorWithCourses(appDAO);
+			//findInstructorWithCourses(appDAO);
+			findCoursesForInstructor(appDAO);
 		};
+	}
+
+	private void findCoursesForInstructor(AppDAO appDAO) {
+		int theId=1;
+		System.out.println("Finding the instructor with id :"+ theId);
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println("the instructor :"+tempInstructor);
+
+		//find courses for instructor
+		System.out.println("Finding courses for instructor ID :"+theId);
+		List<Course> courses = appDAO.findCoursesByInstructorId(theId);
+
+		//associated the objects
+		tempInstructor.setCourses(courses);
+
+		System.out.println("Associated courses :"+tempInstructor.getCourses());
+		System.out.println("Done!");
 	}
 
 	private void findInstructorWithCourses(AppDAO appDAO) {
