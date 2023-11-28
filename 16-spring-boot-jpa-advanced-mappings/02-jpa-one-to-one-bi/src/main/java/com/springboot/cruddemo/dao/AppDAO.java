47b75291b98b4b090667f5a990e112e547ad97cd
krishna <dhekkrishna1@gmail.com>
2023-11-28 19:42:32 +0530

    Creat method findInstructorDetailById in the AppDAO interface

--- 16-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/springboot/cruddemo/dao/AppDAO.java
+++ 16-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/springboot/cruddemo/dao/AppDAO.java
@@ -1,12 +1,19 @@
 package com.springboot.cruddemo.dao;
 
 import com.springboot.cruddemo.entity.Instructor;
+import com.springboot.cruddemo.entity.InstructorDetail;
 
 public interface AppDAO {
     void save(Instructor theInstructor);
 
     Instructor findInstructorById(int theId);
     void deleteInstructorById(int theId);
+
+    //find instructor detail by id
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+
 
 
 }
