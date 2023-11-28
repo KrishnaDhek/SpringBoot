f8a979456cf0231d91bdb6ca33fa1586652fb2b7
krishna <dhekkrishna1@gmail.com>
2023-11-29 01:28:08 +0530

    Initial commit for jpa-one-to-many-uni-directional

---
+++ 16-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/springboot/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,147 @@
+package com.springboot.cruddemo.dao;
+
+import com.springboot.cruddemo.entity.Course;
+import com.springboot.cruddemo.entity.Instructor;
+import com.springboot.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    //define field for entity manager
+
+    //inject entity manager using constructor injection
+
+    private  EntityManager entityManager;
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+    entityManager.persist(theInstructor);
+
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+
+        //retrieve instructor
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+
+        //get courses for instructor
+        List<Course> courses =tempInstructor.getCourses();
+
+
+        //break association of all the courses for the instructor
+
+        for (Course tempCourses: courses){
+            tempCourses.setInstructor(null);
+        }
+
+        //delete the instructor
+        entityManager.remove(tempInstructor);
+
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return  entityManager.find(InstructorDetail.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+
+        //retrieve instructor detail by id
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
+
+        //remove the associated object reference
+        //break bi_directional link
+
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
+
+
+        //delete the instructor detail
+        entityManager.remove(tempInstructorDetail);
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int theId) {
+
+        TypedQuery<Course> query =entityManager.createQuery(
+                "from Course where instructor.id = :data",Course.class);
+        query.setParameter("data", theId);
+
+        //execute
+
+        List<Course> courses = query.getResultList();
+        return courses;
+
+    }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int theId) {
+        TypedQuery<Instructor> query = entityManager.createQuery(
+                                    "select i from Instructor i "
+                                            + "JOIN FETCH i.courses "
+                                            + "JOIN FETCH i.instructorDetail "
+                                            + "where i.id = :data", Instructor.class);
+
+
+        query.setParameter("data" , theId);
+
+        //execute query
+        Instructor instructor = query.getSingleResult();
+        return instructor;
+
+    }
+
+    @Override
+    @Transactional
+    public void update(Instructor theInstructor) {
+        entityManager.merge(theInstructor);
+
+    }
+
+
+    //course update
+    @Override
+    @Transactional
+    public void update(Course theCourse) {
+        entityManager.merge(theCourse);
+    }
+
+    @Override
+    public Course findCourseById(int theID) {
+        return entityManager.find(Course.class, theID);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCourseById(int theId) {
+        //retrieve the course
+       Course tempCourse= entityManager.find(Course.class, theId);
+
+       //delete  the course
+       entityManager.remove(tempCourse);
+
+    }
+
+
+}
---
+++ 16-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/springboot/cruddemo/entity/InstructorDetail.java
@@ -0,0 +1,82 @@
+package com.springboot.cruddemo.entity;
+
+import jakarta.persistence.*;
+import org.springframework.context.annotation.Bean;
+
+//annotate the class as entity and map to the database
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+    //define the fields,annotate the fields with database column name
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    @OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private Instructor instructor;
+
+
+
+    //create constructor
+    public InstructorDetail(){
+
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+    //create getter and setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+    //generate toString() method
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+
+
+
+}
