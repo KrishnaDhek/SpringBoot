3a0d48ec5280427d10e8e9c82655143d6a9cf8bd
krishna <dhekkrishna1@gmail.com>
2023-12-02 22:47:47 +0530

    Updated method name to addSillyAccount in the MembershipDAO interface and MembershipDAOImpl

--- 17-spring-boot-aop/02-spring-boot-aop-pointcut-expressions-match-anyStar-method/src/main/java/com/springboot/aopdemo/dao/MembershipDAO.java
+++ 17-spring-boot-aop/02-spring-boot-aop-pointcut-expressions-match-anyStar-method/src/main/java/com/springboot/aopdemo/dao/MembershipDAO.java
@@ -2,5 +2,5 @@ package com.springboot.aopdemo.dao;
 
 public interface MembershipDAO {
 
-    void addAccount();
+    void addSillyAccount();
 }
--- 17-spring-boot-aop/02-spring-boot-aop-pointcut-expressions-match-anyStar-method/src/main/java/com/springboot/aopdemo/dao/MembershipDAOImpl.java
+++ 17-spring-boot-aop/02-spring-boot-aop-pointcut-expressions-match-anyStar-method/src/main/java/com/springboot/aopdemo/dao/MembershipDAOImpl.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MembershipDAOImpl implements MembershipDAO{
     @Override
-    public void addAccount() {
+    public void addSillyAccount() {
         System.out.println(getClass() +": Doing my DB work: Adding a Membership account");
     }
+
+
 }
