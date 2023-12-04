0858ef840a7111544efcd1f7982647fa48f1f8ad
krishna <dhekkrishna1@gmail.com>
2023-12-04 22:30:39 +0530

    Add MyCloudAsyncAspect for async Cloud logging
    This aspect class, annotated with @Aspect and @Component, is responsible for logging to the Cloud in an async fashion.
    - The @Order(1) annotation specifies the order of execution relative to other aspects.
    - The @Before advice method 'logToCloudAsync' is executed before methods matching the pointcut defined in AOPDeclarations, indicating async Cloud logging.

--- 17-spring-boot-aop/08-spring-boot-aop-demo-order-aspects/src/main/java/com/springboot/aopdemo/aspect/MyCloudAsyncAspect.java
+++ 17-spring-boot-aop/08-spring-boot-aop-demo-order-aspects/src/main/java/com/springboot/aopdemo/aspect/MyCloudAsyncAspect.java
@@ -5,10 +5,21 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+/**
+ * MyCloudAsyncAspect is an aspect class responsible for logging to the Cloud in an async fashion.
+ * It is annotated with @Aspect to indicate that it is an aspect class.
+ * The @Component annotation marks it as a Spring component for auto-discovery.
+ * The @Order(1) annotation specifies the order of execution relative to other aspects.
+ */
 @Aspect
 @Component
 @Order(1)
 public class MyCloudAsyncAspect {
+
+    /**
+     * Advice method executed before methods matching the pointcut defined in AOPDeclarations.
+     * It prints a message indicating logging to the Cloud in an async fashion.
+     */
 
     @Before("com.springboot.aopdemo.aspect.AOPDeclarations.forDaoPackageNoGetterSetter()")
     public void logToCloudAsync() {
