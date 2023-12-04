ec91b08a520b65d48784b2bfb6d58d2373cf9950
krishna <dhekkrishna1@gmail.com>
2023-12-04 22:51:37 +0530

    Enhance MyDemoLoggingAspect to log method signature and arguments
    This aspect class, annotated with @Aspect and @Component, now logs the method signature and arguments before methods in the DAO package (excluding getters and setters).
    - The @Order(2) annotation specifies the order of execution relative to other aspects.
    - The @Before advice method 'beforeAddAccountAdvice' has been enhanced to display the method signature and process method arguments, including special handling for Account objects.

--- 17-spring-boot-aop/09-spring-boot-aop-demo-read-joinpoint/src/main/java/com/springboot/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 17-spring-boot-aop/09-spring-boot-aop-demo-read-joinpoint/src/main/java/com/springboot/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,9 +1,12 @@
 package com.springboot.aopdemo.aspect;
 
 
+import com.springboot.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -23,8 +26,36 @@ public class MyDemoLoggingAspect {
      * It is defined in the AOPDeclarations class.
      */
     @Before("com.springboot.aopdemo.aspect.AOPDeclarations.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
         System.out.println("\n=====>>> Executing @Before advice on method");
+
+        //display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("method :" +methodSignature);
+
+
+        //display the method arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        //get arguments
+
+        for (Object tempArg :args){
+            System.out.println(tempArg);
+
+            if(tempArg instanceof Account){
+
+                //downcast and print Account specific stuff
+                  Account theAcc = (Account) tempArg;
+
+                System.out.println("account name :" +theAcc.getName());
+                System.out.println("account level :" +theAcc.getLevel());
+            }
+        }
+
+        //loop through arguments
+
+
+
     }
 
 }
