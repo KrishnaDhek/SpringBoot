b6b944eb662f9a3ed0683448ec3bb26003059bd6
krishna <dhekkrishna1@gmail.com>
2023-12-04 22:31:44 +0530

    Add MyDemoLoggingAspect for logging before DAO package methods
    This aspect class, annotated with @Aspect and @Component, is responsible for logging before methods in the DAO package (excluding getters and setters).
    - The @Order(2) annotation specifies the order of execution relative to other aspects.
    - The @Before advice method 'beforeAddAccountAdvice' is executed before methods matching the pointcut defined in AOPDeclarations, logging a message before method execution.

--- 17-spring-boot-aop/08-spring-boot-aop-demo-order-aspects/src/main/java/com/springboot/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 17-spring-boot-aop/08-spring-boot-aop-demo-order-aspects/src/main/java/com/springboot/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -7,12 +7,21 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-// An Aspect class to encapsulate cross-cutting concerns like logging
+/**
+ * MyDemoLoggingAspect is an aspect class responsible for logging before methods in the DAO package.
+ * It is annotated with @Aspect to indicate that it is an aspect class.
+ * The @Component annotation marks it as a Spring component for auto-discovery.
+ * The @Order(2) annotation specifies the order of execution relative to other aspects.
+ */
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
 
+    /**
+     * Pointcut declaration for methods in the DAO package excluding getters and setters.
+     * It is defined in the AOPDeclarations class.
+     */
     @Before("com.springboot.aopdemo.aspect.AOPDeclarations.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
