cb7a8534cc29451a2ca5e5abf6ac3d14ea5cd877
krishna <dhekkrishna1@gmail.com>
2024-02-05 18:15:47 +0530

    Created a class Expense with MongoDB annotations

--- spring-boot-mongodb/src/main/java/com/programming/springbootmongodb/model/Expense.java
+++ spring-boot-mongodb/src/main/java/com/programming/springbootmongodb/model/Expense.java
@@ -7,18 +7,29 @@ import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.math.BigDecimal;
 
+// MongoDB Document annotation indicating that instances of this class will be stored in a
+// collection named "expense"
+
 @Document("expense")
 public class Expense {
+    // MongoDB's annotation indicating that this field serves as the primary key in the collection
     @Id
     private String id;
+
+    // MongoDB's annotation specifying the field name in the document and enforcing uniqueness
     @Field(name = "name")
     @Indexed(unique = true)
     private String expenseName;
+
+    // Enumeration representing different categories of expenses
     @Field(name = "category")
     private ExpenseCategory expenseCategory;
+
+    // BigDecimal field to store the amount of the expense
     @Field(name = "amount")
     private BigDecimal expenseAmount;
 
+    // Constructor to initialize the Expense object
     public Expense(String id, String expenseName, ExpenseCategory expenseCategory, BigDecimal expenseAmount) {
         this.id = id;
         this.expenseName = expenseName;
@@ -26,6 +37,7 @@ public class Expense {
         this.expenseAmount = expenseAmount;
     }
 
+    //Getter and Setter method for retrieving the expense
     public String getId() {
         return id;
     }
