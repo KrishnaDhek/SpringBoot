ce5075e96789d6f85035a5ee1e707ccb94a2f197
krishna <dhekkrishna1@gmail.com>
2024-02-05 18:31:35 +0530

    Implement ExpenseController for managing expense endpoints - : POST endpoint to add a new expense. - : PUT endpoint to update an existing expense. - : GET endpoint to retrieve a list of all expenses. - : GET endpoint to retrieve an expense by name. - : DELETE endpoint to delete an expense by ID.

--- spring-boot-mongodb/src/main/java/com/programming/springbootmongodb/controller/ExpenseController.java
+++ spring-boot-mongodb/src/main/java/com/programming/springbootmongodb/controller/ExpenseController.java
@@ -1,25 +1,34 @@
 package com.programming.springbootmongodb.controller;
 
 import com.programming.springbootmongodb.model.Expense;
-import com.programming.springbootmongodb.repository.ExpenseRepository;
 import com.programming.springbootmongodb.service.ExpenseService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
+
 
 @RestController
 @RequestMapping("/api/expense")
 public class ExpenseController {
+
+    /**
+     * ExpenseController Constructor
+     *
+     * This constructor initializes the ExpenseController with an instance of ExpenseService.
+     * The ExpenseService is injected to facilitate communication between the controller
+     * and the underlying service layer for managing expense-related operations.
+     *
+     * @param expenseService The ExpenseService responsible for handling business logic and data operations.
+     */
     private final ExpenseService expenseService;
 
     public ExpenseController(ExpenseService expenseService){
         this.expenseService = expenseService;
     }
 
+    //Endpoint to add a new expense
     @PostMapping
     public ResponseEntity addExpense(@RequestBody Expense expense){
         expenseService.addExpense(expense);
@@ -27,6 +36,7 @@ public class ExpenseController {
 
     }
 
+    //Endpoint to update an existing expense
     @PutMapping
     public ResponseEntity<Void> updateExpense(@RequestBody Expense expense){
         expenseService.updateExpense(expense);
@@ -34,17 +44,21 @@ public class ExpenseController {
 
     }
 
+    //Endpoint to retrieve all the expenses
     @GetMapping
     public ResponseEntity<List<Expense>>getAllExpense(){
         return ResponseEntity.ok(expenseService.getAllExpense());
     }
 
+    //Endpoint to retrieve an expense by name
     @GetMapping("/{name}")
     public ResponseEntity getExpenseByName(@PathVariable String name){
 
         return ResponseEntity.ok(expenseService.getExpense(name));
     }
 
+
+    //Endpoint to delete an expense by ID
     @DeleteMapping("/{id}")
     public ResponseEntity deleteExpense(@PathVariable  String id){
         expenseService.deleteExpense(id);
