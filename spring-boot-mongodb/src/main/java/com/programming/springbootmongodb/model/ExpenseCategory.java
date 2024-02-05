8947115ce5b1b314506bfa50231afb7324c3b80a
krishna <dhekkrishna1@gmail.com>
2024-02-05 18:17:45 +0530

    Add ExpenseCategory enum representing different expense categories

--- spring-boot-mongodb/src/main/java/com/programming/springbootmongodb/model/ExpenseCategory.java
+++ spring-boot-mongodb/src/main/java/com/programming/springbootmongodb/model/ExpenseCategory.java
@@ -1,5 +1,16 @@
 package com.programming.springbootmongodb.model;
+/**
+ * Enumeration representing different categories for expenses.
+ *
+ * - ENTERTAINMENT: Expenses related to entertainment activities.
+ * - GROCERIES: Expenses related to grocery shopping.
+ * - RESTAURANTS: Expenses incurred at restaurants.
+ * - UTILITIES: Expenses related to utility bills.
+ * - MISC: Miscellaneous expenses that don't fall into specific categories.
+ */
 
 public enum ExpenseCategory {
+
+
     ENTERTAINMENT, GROCERIES, RESTAURANTS, UTILITIES, MISC
 }
