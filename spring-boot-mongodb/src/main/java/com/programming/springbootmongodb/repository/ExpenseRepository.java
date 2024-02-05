56d7a618485e15f5aa29784b42dbe5d3a40a7722
krishna <dhekkrishna1@gmail.com>
2024-02-05 18:19:10 +0530

    Add custom query method to ExpenseRepository to find expense by name

--- spring-boot-mongodb/src/main/java/com/programming/springbootmongodb/repository/ExpenseRepository.java
+++ spring-boot-mongodb/src/main/java/com/programming/springbootmongodb/repository/ExpenseRepository.java
@@ -7,7 +7,12 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.Optional;
 
 public interface ExpenseRepository extends MongoRepository<Expense,String > {
-
+    /**
+     * Custom query method to find an expense by its name.
+     *
+     * @param name The name of the expense to search for.
+     * @return An optional containing the expense if found, or empty if not.
+     */
     @Query("{'name': ?0}")
     Optional<Expense> findByName(String name);
 
