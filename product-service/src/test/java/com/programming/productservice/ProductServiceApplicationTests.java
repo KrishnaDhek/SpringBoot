574e2e4efceb274f13d9e7299cfb633d58184720
krishna <dhekkrishna1@gmail.com>
2024-02-06 15:21:55 +0530

    Remove product-service

--- product-service/src/test/java/com/programming/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/programming/productservice/ProductServiceApplicationTests.java
@@ -3,7 +3,13 @@ package com.programming.productservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
+
 @SpringBootTest
+@Testcon
+
+
 class ProductServiceApplicationTests {
 
 	@Test
