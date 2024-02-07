70280d84e60702ce268812a82fdd90d333379536
krishna <dhekkrishna1@gmail.com>
2024-02-07 23:36:23 +0530

    Removed product-service folder

--- product-service/src/main/java/com/programming/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.programming.productservice.repository;
-
-import com.programming.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-/**
- * Repository interface for managing Product entities in MongoDB.
- */
-public interface ProductRepository extends MongoRepository<Product,String > {
-}
