b369089128e7f88021b42c0878c002da3ee632b9
krishna <dhekkrishna1@gmail.com>
2024-02-08 04:38:28 +0530

    Added initial data using CommandLineRunner for InventoryService

--- spring_microservice/inventory-service/src/main/java/com/programming/inventoryservice/InventoryServiceApplication.java
+++ spring_microservice/inventory-service/src/main/java/com/programming/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.programming.inventoryservice;
 
+import com.programming.inventoryservice.model.Inventory;
+import com.programming.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +13,32 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("book");
+			inventory1.setQuantity(150);
+
+			Inventory inventory2= new Inventory();
+			inventory2.setSkuCode("bedsheets");
+			inventory2.setQuantity(150);
+
+			Inventory inventory3 = new Inventory();
+			inventory3.setSkuCode("shoes");
+			inventory3.setQuantity(150);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+			inventoryRepository.save(inventory3);
+		};
+	}
+
 
 }
