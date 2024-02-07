e24fe49ac06d2c02b433348855d82b4c981e1a0e
krishna <dhekkrishna1@gmail.com>
2024-02-08 02:00:25 +0530

    Updated the @PostMapping for end point placeOrder by adding HttpStatus

--- spring_microservice/order-service/src/main/java/com/prodramming/orderservice/controller/OrderController.java
+++ spring_microservice/order-service/src/main/java/com/prodramming/orderservice/controller/OrderController.java
@@ -1,17 +1,27 @@
 package com.prodramming.orderservice.controller;
 
 import com.prodramming.orderservice.dto.OrderRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.prodramming.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+    /**
+     * Endpoint to place a new order.
+     *
+     * @param orderRequest The request containing order details.
+     * @return A message indicating successful order placement.
+     */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
     }
 }
